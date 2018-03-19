8543195a1fc1b37cb2bf21918b033b62c53d9a31
dnks2000@gmail.com <nelum.sam@goldfeinclaims.com>
2018-03-19 15:51:04 -0400

    :smirk: db testing done

--- test/test-server.js
+++ test/test-server.js
@@ -18,6 +18,7 @@ chai.use(chaiHttp);
 
 
 describe('Blobs', function() {
+  this.timeout(15000);
 
   // Delete the table before start
   db.Blob.destroy({
@@ -34,14 +35,14 @@ describe('Blobs', function() {
     done();
   });
 
-  // // Destroy the record
-  // afterEach(function(done){
-  //   db.Blob.destroy({
-  //     where: {},
-  //     truncate: true
-  //   })
-  //   done();
-  // });
+  // Destroy the record
+  afterEach(function(done){
+    db.Blob.destroy({
+      where: {},
+      truncate: true
+    })
+    done();
+  });
 
   it('should list ALL blobs on /blobs GET', function(done) {
     chai.request(url)
@@ -59,88 +60,68 @@ describe('Blobs', function() {
       });
   });
 
-  // it('should list a SINGLE blob on /blob/<id> GET', function(done) {
-  //     var newBlob = new Blob({
-  //       name: 'Super',
-  //       lastName: 'man'
-  //     });
-  //     newBlob.save(function(err, data) {
-  //       chai.request(server)
-  //         .get('/blob/'+data.id)
-  //         .end(function(err, res){
-  //           res.should.have.status(200);
-  //           res.should.be.json;
-  //           res.body.should.be.a('object');
-  //           res.body.should.have.property('_id');
-  //           res.body.should.have.property('name');
-  //           res.body.should.have.property('lastName');
-  //           res.body.name.should.equal('Bat');
-  //           res.body.lastName.should.equal('man');
-  //           res.body._id.should.equal(data.id);
-  //           done();
-  //         });
-  //     });
-  // });
+  it('should add a SINGLE blob on /blobs POST', function(done) {
+    chai.request(url)
+      .post('/blobs')
+      .send({'name': 'Java', 'lastName': 'Script'})
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('SUCCESS');
+        res.body.SUCCESS.should.be.a('object');
+        res.body.SUCCESS.should.have.property('name');
+        res.body.SUCCESS.should.have.property('lastName');
+        res.body.SUCCESS.should.have.property('_id');
+        res.body.SUCCESS.name.should.equal('Java');
+        res.body.SUCCESS.lastName.should.equal('Script');
+        done();
+      });
+      done();
+  });
 
-  // it('should add a SINGLE blob on /blobs POST', function(done) {
-  //   chai.request(server)
-  //     .post('/blobs')
-  //     .send({'name': 'Java', 'lastName': 'Script'})
-  //     .end(function(err, res){
-  //       res.should.have.status(200);
-  //       res.should.be.json;
-  //       res.body.should.be.a('object');
-  //       res.body.should.have.property('SUCCESS');
-  //       res.body.SUCCESS.should.be.a('object');
-  //       res.body.SUCCESS.should.have.property('name');
-  //       res.body.SUCCESS.should.have.property('lastName');
-  //       res.body.SUCCESS.should.have.property('_id');
-  //       res.body.SUCCESS.name.should.equal('Java');
-  //       res.body.SUCCESS.lastName.should.equal('Script');
-  //       done();
-  //     });
-  // });
+  it('should update a SINGLE blob on /blob/<id> PUT', function(done) {
+    chai.request(url)
+      .get('/blobs')
+      .end(function(err, res){
+        chai.request(url)
+          .put('/blob/1') // + res.body[0].id)
+          .send({'name': 'Spider'})
+          .end(function(error, response){
+            response.should.have.status(200);
+            response.should.be.json;
+            response.body.should.be.a('object');
+            response.body.should.have.property('UPDATED');
+            response.body.UPDATED.should.be.a('object');
+            response.body.UPDATED.should.have.property('name');
+            response.body.UPDATED.should.have.property('id');
+            response.body.UPDATED.name.should.equal('Spider');
+            done();
+        });
+      });
+      done();
+  });
 
-  // it('should update a SINGLE blob on /blob/<id> PUT', function(done) {
-  //   chai.request(server)
-  //     .get('/blobs')
-  //     .end(function(err, res){
-  //       chai.request(server)
-  //         .put('/blob/'+res.body[0]._id)
-  //         .send({'name': 'Spider'})
-  //         .end(function(error, response){
-  //           response.should.have.status(200);
-  //           response.should.be.json;
-  //           response.body.should.be.a('object');
-  //           response.body.should.have.property('UPDATED');
-  //           response.body.UPDATED.should.be.a('object');
-  //           response.body.UPDATED.should.have.property('name');
-  //           response.body.UPDATED.should.have.property('_id');
-  //           response.body.UPDATED.name.should.equal('Spider');
-  //           done();
-  //       });
-  //     });
-  // });
-
-  // it('should delete a SINGLE blob on /blob/<id> DELETE', function(done) {
-  //   chai.request(server)
-  //     .get('/blobs')
-  //     .end(function(err, res){
-  //       chai.request(server)
-  //         .delete('/blob/'+res.body[0]._id)
-  //         .end(function(error, response){
-  //           response.should.have.status(200);
-  //           response.should.be.json;
-  //           response.body.should.be.a('object');
-  //           response.body.should.have.property('REMOVED');
-  //           response.body.REMOVED.should.be.a('object');
-  //           response.body.REMOVED.should.have.property('name');
-  //           response.body.REMOVED.should.have.property('_id');
-  //           response.body.REMOVED.name.should.equal('Bat');
-  //           done();
-  //       });
-  //     });
-  // });
+  it('should delete a SINGLE blob on /blob/<id> DELETE', function(done) {
+    chai.request(url)
+      .get('/blobs')
+      .end(function(err, res){
+        chai.request(url)
+          .delete('/blob/1') //+res.body[0]._id)
+          .end(function(error, response){
+            response.should.have.status(200);
+            response.should.be.json;
+            response.body.should.be.a('object');
+            response.body.should.have.property('REMOVED');
+            response.body.REMOVED.should.be.a('object');
+            response.body.REMOVED.should.have.property('name');
+            response.body.REMOVED.should.have.property('id');
+            response.body.REMOVED.name.should.equal('Wonder');
+            done();
+        });
+      });
+      done();
+  });
 
 });
 
